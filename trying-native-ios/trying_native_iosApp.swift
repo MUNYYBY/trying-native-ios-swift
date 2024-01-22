980a0c8354c46cc065aae92f22187bf3561a8bc3
MUNYYBY <hm.muneeb.u.r@gmail.com>
2024-01-22 11:56:57 +0500

    Initial Commit

---
+++ trying-native-ios/trying_native_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  trying_native_iosApp.swift
+//  trying-native-ios
+//
+//  Created by Munyyb on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct trying_native_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
